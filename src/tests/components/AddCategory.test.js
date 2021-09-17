3ef74d6922356dbe7af94561d10f72e22860340b
sefhirot69 <josalillo@gmail.com>
2021-09-17 13:07:07 +0200

    feat : test para comprobar que el input cambia de categoria

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -3,11 +3,20 @@ import AddCategory from "../../components/AddCategory";
 
 describe('Pruebas sobre AddCategory', () => {
 
-    const setCategories = () => {};
+    const setCategories = () => {
+    };
     const wrapper = shallow(<AddCategory setCategories={setCategories}/>)
 
     test('Debe mostrar el componente AddCategory', () => {
 
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe cambiar el input de categoria', () => {
+
+        const input = wrapper.find('input');
+        const value = 'Hola mundo';
+
+        input.simulate('change', {target: {value}});
     });
 });
