15898d315492b589fc4e1ba3f809224a7cc1fd47
sefhirot69 <josalillo@gmail.com>
2021-09-20 11:06:32 +0200

    feat : test sobre la funcion handleSubmit al completo

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -26,9 +26,27 @@ describe('Pruebas sobre AddCategory', () => {
 
     test('No debe de postear la informacion con submit', () => {
 
-        const submit = wrapper.find('form').simulate('submit',{ preventDefault: () => {}});
+        wrapper.find('form').simulate('submit',{ preventDefault: () => {}});
 
         expect(setCategories).not.toHaveBeenCalled();
     });
+
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+
+        const input = wrapper.find('input');
+        const value = 'Goku';
+
+        input.simulate('change', {target: {value}});
+        wrapper.find('form').simulate('submit',{ preventDefault: () => {}});
+
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        //Estoy esperando que se haya llamado con cualquier tipo de función
+        expect(setCategories).toHaveBeenCalledWith( expect.any(Function));
+        expect(input.prop('value')).toBe('');
+
+
+    });
+
 
 });
